1b8889f851c4acc915b90ea105a6804c6b061cfa
jakelsaunders94 <jakelsaunders94@gmail.com>
2021-08-16 16:03:51 +0100

    modify main setup

--- django_migration_dbindex_check/main.py
+++
@@ -1,8 +0,0 @@
-from .checker import DBIndexChecker
-import sys
-
-if __name__ == '__main__':
-    checker = DBIndexChecker()
-    checker.check_project(sys.argv[0])
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
